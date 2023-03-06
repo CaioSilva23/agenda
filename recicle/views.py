704d38fc7b7c0ea5fb059db42281510b5b4cc733
caio silva <caio_ceac23@hotmail.com>
2023-03-06 11:41:37 -0300

    cbv

--- recicle/views.py
+++ recicle/views.py
@@ -1,75 +1,90 @@
-# from django.shortcuts import render, get_object_or_404, redirect
-# from .models import Contato
+
+# from django.views.generic import View, TemplateView, ListView, DetailView
+# from django.shortcuts import redirect, get_object_or_404,render
+# from contatos.forms import FormContato
+# from django.contrib import messages
+# from contatos.models import Contato
+# from django.contrib.auth.decorators import login_required
+# from django.utils.decorators import method_decorator
+# from django.http import Http404, HttpResponse
 # from django.core.paginator import Paginator
 # from django.db.models import Q, Value
 # from django.db.models.functions import Concat
-# from django.contrib.auth.decorators import login_required
-# from django.contrib import messages
-# from .forms import FormContato
 
-# @login_required(login_url='login')
-# def home(request):
-#     form = FormContato()
-#     contatos = Contato.objects.order_by('nome').filter(user=request.user)
-#     pesquisa = request.POST.get('pesquisa')
+
+# @method_decorator(
+#     login_required(login_url='login', redirect_field_name='next'),
+#     name='dispatch'
+# )
+
+
+
+# class ContatosDetail(DetailView):
+#     model = Contato
+#     context_object_name = 'contato'
+#     template_name = 'contatos/contato.html'
     
-#     campos = Concat('nome', Value(' '), 'sobrenome')
-
-#     if pesquisa:
-#         contatos = contatos.annotate(nome_completo=campos).filter(Q(nome_completo__icontains=pesquisa) | Q(telefone__icontains=pesquisa))
-
-#     # PAGINAÇÃO 
-#     paginator = Paginator(contatos, 6)
-#     page = request.GET.get('p')
-#     contatos = paginator.get_page(page)
-
-#     return render(request, 'contatos/home.html', {'contatos': contatos, 'form':form})
-
-
-# def contato(request, id):
-#     contato = get_object_or_404(Contato, id=id)
-#     form = FormContato(instance=contato)
-#     if request.user != contato.user:
-#         messages.info(request, 'Esse contato não é seu!')
-#         return redirect('home')
-#     return render(request, 'contatos/contato.html', {'contato': contato,'form': form})
-
-
-# def novo_contato(request):
-#     if request.method == 'GET':
-#         return redirect('home')
-#     elif request.method == 'POST':
-#         form = FormContato(request.POST, request.FILES)
+#     def get_context_data(self, *args, **kwargs):
+#         ctx = super().get_context_data(*args, **kwargs)
+#         form = FormContato(instance=self.get_object())
+#         ctx.update({'form': form})
+#         return ctx
+    
+#     def post(self, *args, **kwargs):
+#         form = FormContato(self.request.POST or None, 
+#                             self.request.FILES or None, 
+#                             instance=self.get_object())
 #         if form.is_valid():
 #             user = form.save(commit=False)
-#             user.user = request.user
+#             user.user = self.request.user
 #             user.save()
-#             messages.success(request, 'Contato cadastrado com sucesso!')
-#             return redirect('home')
+#             messages.success(self.request, 'Contato editado com sucesso!')      
+#             return redirect(f'/contato/{self.get_object().id}/')
 #         else:
-#             return redirect('home')
+#             messages.info(self.request, 'Dados inválidos, tente novamente!')  
+#             return redirect(f'/contato/{self.get_object().id}/')
 
 
-# def editar_contato_id(request, id):
-#     contato = get_object_or_404(Contato, id=id)
-
-#     if request.method == 'GET':
-#         return redirect('home')
+# class ContatosList(ListView, Paginator):
+#     model = Contato
+#     template_name = 'contatos/home.html'
+#     context_object_name = 'contatos'
+#     ordering = 'nome'
     
-#     elif request.method == 'POST':
-#         form = FormContato(request.POST, request.FILES, instance=contato)
+#     def paginater(self, contatos):
+#         paginator = Paginator(contatos, 6)
+#         page = self.request.GET.get('p')
+#         contatos = paginator.get_page(page)
+#         return contatos
+
+#     def get_context_data(self, *args, **kwargs):
+#         ctx = super().get_context_data(*args, **kwargs)
+#         form = FormContato()
+     
+#         contatos = self.paginater(self.get_queryset())
+#         ctx['contatos'] = contatos
+#         ctx.update({'form': form})
+#         print(ctx)
+#         return ctx
+    
+#     def post(self, *args, **kwargs):
+#         form = FormContato(self.request.POST or None, 
+#                             self.request.FILES or None, 
+#                             )
 #         if form.is_valid():
 #             user = form.save(commit=False)
-#             user.user = request.user
+#             user.user = self.request.user
 #             user.save()
-#             messages.success(request, 'Contato editado com sucesso!')
-#             return redirect(f'/contatos/contato/{id}')
+#             messages.success(self.request, 'Contato salvo com sucesso!')  
+#             return redirect('home')
 #         else:
+#             messages.error(self.request, 'Error ao salvar o contato, tente novamente!')  
 #             return redirect('home')
 
 
-# def deletar(request, id):
-#     contato = get_object_or_404(Contato, id=id)
-#     contato.delete()
-#     messages.success(request, 'Contato deletado com sucesso!')
-#     return redirect('home')
+# class ContatoDelete(ContatosDetail):
+#     def get(self, *args, **kwargs):
+#         contato = Contato.objects.get(id=kwargs['pk'])
+#         contato.delete()
+#         messages.success(self.request, 'Contato deletado com sucesso!')
+#         return redirect('home')
